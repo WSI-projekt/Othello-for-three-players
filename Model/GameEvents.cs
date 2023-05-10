80ce76f1f0a9f25026be8835ee7598048d755814
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-10 02:09:06 +0200

    Add general `GameController`

---
+++ Model/GameEvents.cs
@@ -0,0 +1,44 @@
+﻿using Othello_for_three_players.Model.Players;
+
+namespace Othello_for_three_players.Model
+{
+    public class GameEvent { }
+
+    public class GameHaveStaretd : GameEvent { }
+
+    public class MoveWasMade
+    {
+        public Board OldBoard { get; }
+        public Move Move { get; }
+        public Board Board { get; }
+
+        public MoveWasMade(Board oldBoard, Move move, Board board)
+        {
+            OldBoard = oldBoard;
+            Move = move;
+            Board = board;
+        }
+    }
+
+    public class GameFinished: GameEvent { }
+
+    public class Draw: GameFinished
+    {
+        public IReadOnlyCollection<PlayerID> Winners { get; }
+
+        public Draw(IReadOnlyCollection<PlayerID> winners)
+        {
+            Winners = winners;
+        }
+    }
+
+    public class PlayerWon: GameFinished
+    {
+        public PlayerID Winner { get; }
+
+        public PlayerWon(PlayerID winner)
+        {
+            Winner = winner;
+        }
+    }
+}
