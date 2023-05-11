903209c0cbd2548df2b875fc0b888d12255ee5fc
Piotr Nieciecki <piotr.nieciecki01@gmail.com>
2023-05-11 18:48:09 +0200

    Add `Queue` concatenation

---
+++ Model/Utils/QueueConcatenation.cs
@@ -0,0 +1,17 @@
+﻿namespace Othello_for_three_players.Model.Utils
+{
+    public static class QueueConcatenation
+    {
+        /// <summary>
+        /// Moves all elements form `q`
+        /// </summary>
+        /// <typeparam name="T">Data type</typeparam>
+        /// <param name="destQ">Destination queue</param>
+        /// <param name="q">Source queue</param>
+        public static void Concatenate<T>(this Queue<T> destQ, Queue<T> q)
+        {
+            while(q.Count > 0)
+                destQ.Enqueue(q.Dequeue());
+        }
+    }
+}
