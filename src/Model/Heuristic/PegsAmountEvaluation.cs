b80a20c0f91b4d6fb87e91f7a03e9f7ed23ef259
Peter-worm <1234ozmo@wp.pl>
2023-05-22 12:26:06 +0200

    aded staic table and fixed pegsamountevaluation

--- src/Model/Heuristic/PegsAmountEvaluation.cs
+++ src/Model/Heuristic/PegsAmountEvaluation.cs
@@ -4,18 +4,6 @@
     {
         Board board;
 
-        int[,] evaluationtab = new int[9, 9] {
-            {  4, -3,  2,  2,  2,  2,  2, -3,  4 },
-            { -3, -4, -1, -1, -1, -1, -1, -4, -3 },
-            {  2, -1,  1,  0,  0,  0,  1, -1,  2 },
-            {  2, -1,  0,  1,  1,  1,  0, -1,  2 },
-            {  2, -1,  0,  1,  1,  1,  0, -1,  2 },
-            {  2, -1,  0,  1,  1,  1,  0, -1,  2 },
-            {  2, -1,  1,  0,  0,  0,  1, -1,  2 },
-            { -3, -4, -1, -1, -1, -1, -1, -4, -3 },
-            {  4, -3,  2,  2,  2,  2,  2, -3,  4 },
-        };
-
         public PegsAmountEvaluation(Board b)
         {
             board = b;
@@ -29,8 +17,10 @@
                 for(int j = 0; j < Board.Size; j++)
                 {
                     int temp = (int)board[i,j] - 1;
-                    if (temp != -1) { }
-                    tab[temp]+= evaluationtab[i,j];
+                    if (temp != -1)
+                    {
+                        tab[temp] += 1;
+                    }
                 }
             }
             tab[0] = tab[0] - (tab[1] + tab[2]) / 2.0;
--- src/Model/Heuristic/StabilityEvaluation.cs
+++ src/Model/Heuristic/StabilityEvaluation.cs
@@ -4,6 +4,18 @@
     {
         Board board;
 
+        int[,] evaluationtab = new int[9, 9] {
+            {  4, -3,  2,  2,  2,  2,  2, -3,  4 },
+            { -3, -4, -1, -1, -1, -1, -1, -4, -3 },
+            {  2, -1,  1,  0,  0,  0,  1, -1,  2 },
+            {  2, -1,  0,  1,  1,  1,  0, -1,  2 },
+            {  2, -1,  0,  1,  1,  1,  0, -1,  2 },
+            {  2, -1,  0,  1,  1,  1,  0, -1,  2 },
+            {  2, -1,  1,  0,  0,  0,  1, -1,  2 },
+            { -3, -4, -1, -1, -1, -1, -1, -4, -3 },
+            {  4, -3,  2,  2,  2,  2,  2, -3,  4 },
+        };
+
         public StabilityEvaluation(Board board)
         {
             this.board = board;
@@ -11,7 +23,19 @@
 
         public override (double a, double b, double c) Evaluate()
         {
-            double[] tab = new double[2];
+            double[] tab = new double[3];
+
+            for (int i = 0; i < Board.Size; i++)
+            {
+                for (int j = 0; j < Board.Size; j++)
+                {
+                    int temp = (int)board[i, j] - 1;
+                    if (temp != -1)
+                    {
+                        tab[temp] += evaluationtab[i, j];
+                    }
+                }
+            }
 
             tab[0] = tab[0] - (tab[1] + tab[2]) / 2.0;
             tab[1] = tab[1] - (tab[0] + tab[2]) / 2.0;
